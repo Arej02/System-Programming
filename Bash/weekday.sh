c13bbe2ed2c4a7dfccfe63a5da8cf58edd8806ce
Arya Raj Khadka <159859689+Arej02@users.noreply.github.com>
2025-06-24 22:46:43 +0930

    Add files via upload

---
+++ Bash/weekday.sh
@@ -0,0 +1,13 @@
+
+number=$1
+
+case $number in
+    1) echo "Mon" ;;
+    2) echo "Tue" ;;
+    3) echo "Wed" ;;
+    4) echo "Thu" ;;
+    5) echo "Fri" ;;
+    6) echo "Sat" ;;
+    7) echo "Sun" ;;
+    *) echo "Enter valid number" ;;
+esac
